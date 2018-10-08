26716fe2fb71d8dda61d6ed462641c3242439e91
EgorNikitin <nicita1221@yandex.ru>
2018-10-08 15:44:42 +0400

    create test program

---
+++ include/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* True if compiler supports -fvisibility=hidden */
+#define HAVE_VISIBILITY_HIDDEN 1
+
+/* Name of package */
+#define PACKAGE "libnetfilter_queue"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libnetfilter_queue"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libnetfilter_queue 1.0.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libnetfilter_queue"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.3"
+
+/* Version number of package */
+#define VERSION "1.0.3"
